fbfb0c1c7d9cfb2709f033a38930f73333fee0df
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-26 01:12:05 +0000

    added stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina peighambarzadeh
+
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+const randomNumber = Math.floor(Math.random() * 6) + 1
+/*
+* This function calculates the area of a triangle.
+*/
+function check() {
+  // input
+  const userNumber =  parseInt(document.getElementById("user-number").value)
+
+  // process
+  if (userNumber == randomNumber) {
+    // output
+  document.getElementById("result").innerHTML =
+    "You have gussed the correct number!"
+}
+
+  if (userNumber != randomNumber) {
+    //output
+    document.getElementById("result"). innerHTML =
+    "You have gussed the wrong number!"
+  }
+}
